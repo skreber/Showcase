696d807e97db4139e0acdd2a7534f12cb05144f1
skreber <sergejkreber@mail.ru>
2018-03-11 12:00:30 +0100

    ECM-245 Create organize flight module

--- frontend/src/app/shipment/caseUI/caseUI.module.ts
+++ frontend/src/app/shipment/caseUI/caseUI.module.ts
@@ -2,11 +2,14 @@ import {LeftSideBarModule} from "./caseUI-leftSideBar/leftSideBar.module";
 import {NgModule} from "@angular/core";
 import {CaseUiComponent} from "./caseUi.component";
 import {CaseUIShipmentDetailModule} from "./caseUI-shipmentDetail/caseUI-shipmentDetail.module";
+import {CaseUICenterAreaComponent} from "./caseUI-centerArea/caseUI-centerArea.component";
+import {CaseUICenterAreaModule} from "./caseUI-centerArea/caseUI-centerArea.module";
 
 @NgModule({
   imports: [
     LeftSideBarModule,
-    CaseUIShipmentDetailModule
+    CaseUIShipmentDetailModule,
+    CaseUICenterAreaModule
   ],
   declarations: [
     CaseUiComponent
